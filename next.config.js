d8657068f526a1d097d9df3512312ac69f0b37e8
RicardoSantosSantana <ricardo@needsolution.com.br>
2021-12-18 17:07:41 -0300

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/faq',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+      {
+        source: '/api/exercicio',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      }
+    ] 
+  }
 }
